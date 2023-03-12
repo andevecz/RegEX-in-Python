1eab31a5d3bf62b88a750add879d4ecd6b9042d1
André Devecz <101411270+andevecz@users.noreply.github.com>
2023-03-12 15:13:15 -0300

    Now the program shows the CPF!

--- regex-cpf.py
+++ regex-cpf.py
@@ -1,7 +1,7 @@
 #A simple Python program to test the usage of RegEx.
 import re
 
-def cpfTester(cpf):
+def CpfTester(cpf):
 
     if bool(re.search(r'\b[0-9]{3}\.+[0-9]{3}\.+[0-9]{3}-+[0-9]{2}\b', cpf)) == True:
         return True
@@ -9,11 +9,13 @@ def cpfTester(cpf):
         return False
 
 cpf = ""
+cpfList = []
 
-cpf = input("Write any CPF(Cadastro de Pessoa Física) with the dots and slashes (e.g., 123.456.789-01):")
+cpf = input("Write a sentence containing any CPF(Cadastro de Pessoa Física) with the dots and slashes (e.g., 123.456.789-01):")
 
-if cpfTester(cpf):
-    print("The CPF is valid.")
+if CpfTester(cpf):
+    cpfList = re.findall(r'\b[0-9]{3}\.+[0-9]{3}\.+[0-9]{3}-+[0-9]{2}\b', cpf)
+    print("The CPF is valid, and it is: " + cpfList[0])
 else:
     print("The CPF is invalid.")
 
