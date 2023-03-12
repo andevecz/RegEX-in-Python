c78241a11ed51e98deade50b0a7367ae5580812f
André Devecz <101411270+andevecz@users.noreply.github.com>
2023-03-12 16:09:54 -0300

    Now the program lists ALL the CPFs that were found

--- regex-cpf.py
+++ regex-cpf.py
@@ -10,14 +10,21 @@ def CpfTester(cpf):
 
 cpf = ""
 cpfList = []
+i = 0
 
 cpf = input("Write a sentence containing any CPF(Cadastro de Pessoa Física) with the dots and slashes (e.g., 123.456.789-01):")
 
 if CpfTester(cpf):
     cpfList = re.findall(r'\b[0-9]{3}\.+[0-9]{3}\.+[0-9]{3}-+[0-9]{2}\b', cpf)
-    print("The CPF is valid, and it is: " + cpfList[0])
+    if len(cpfList) == 1:
+        print("1 valid CPF was found, and it is: " + cpfList[0])
+    else:
+        print(str(len(cpfList)) + " valid CPFs were found, and they are: ")
+        while i < len(cpfList):
+            print(cpfList[i])
+            i += 1
 else:
-    print("The CPF is invalid.")
+    print("No valid CPF was found.")
 
 input("Press 'ENTER' to continue.")
 
